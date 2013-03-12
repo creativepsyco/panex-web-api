14cfe54a932504862daf3c4132babccf19188dd9
Mohit Kanwal <mohit.kanwal@gmail.com>
2013-03-12 13:55:54 +0800

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PanexWebApi::Application.config.secret_token = '48c39c7441cb7563b3a202e1ff41f1cfbfe7d1b4c62e5905a5bdae812bad3bd1b1cf1c8809b4fc0a4d39211c63b410890a81072c52fc210fb83589ced568c942'
