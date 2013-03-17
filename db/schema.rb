4f7c259373dcafae389e89c888d780fe2440c1bb
Mohit Kanwal <mohit.kanwal@gmail.com>
2013-03-18 03:15:51 +0800

    schema after migrating patients

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(:version => 20130313082712) do
   add_index "patients", ["lastName"], :name => "index_patients_on_lastName"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",          :null => false
-    t.string   "encrypted_password",     :default => "",          :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(:version => 20130313082712) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                      :null => false
-    t.datetime "updated_at",                                      :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
-    t.string   "role",                   :default => "clinician"
-    t.string   "name",                   :default => "",          :null => false
+    t.string   "role"
+    t.string   "name",                   :default => "", :null => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
